f5dcf49229491f26cad16780432decc5e848f57a
PolinaShevtsova <pes4@tpu.ru>
2023-07-17 22:46:43 +0700

    Pt_3_3: Checking whether the number is even
    Fixed incorrect operation of the program.

--- Pt_3_3.py
+++ Pt_3_3.py
@@ -1,5 +1,6 @@
-lisst = [int(input("Введите число: "))]
-if lambda x: x % 2 == 0:
+lisst = int(input("Введите число: "))
+f = lambda x: x % 2 == 0
+if f(lisst):
     print("Число четное")
 else:
     print("Число нечетное")
