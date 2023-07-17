fb650d7c6e38fa464b6c86d3fbaeb2b519019c06
PolinaShevtsova <pes4@tpu.ru>
2023-07-18 00:16:51 +0700

    Pt_4_2: Palindromes of a string
    Correct the mistake

--- Pt_4_2.py
+++ Pt_4_2.py
@@ -1,7 +1,7 @@
 def find_palindromes(s):
     palindromes = []
     for i in range(len(s)):
-        for j in range(i+1, len(s)+1):
+        for j in range(i + 1, len(s) + 1):
             stroke = s[i:j]
             reversed_stroke = "".join(reversed(stroke))
             if stroke == reversed_stroke:
